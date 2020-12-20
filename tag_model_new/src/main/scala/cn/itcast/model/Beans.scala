32fbccae3cff182b94e70d37fda3fefeeeedb39a
mrzhang638993 <1446719386@qq.com>
2020-12-20 21:34:03 +0800

    完成获取元数据的相关的操作实现

---
+++ tag_model_new/src/main/scala/cn/itcast/model/Beans.scala
@@ -0,0 +1,116 @@
+package cn.itcast.model
+
+import org.apache.commons.lang3.StringUtils
+
+/**
+ * 所有的标签对应的样例类对象。
+ * */
+case class  Tag(id:String,name:String,rule:String,pid:String)
+
+/**
+ * 元数据的样例类信息
+ * 支持的原始数据库的操作的。支持mysql,hdfs文件，hbase文件数据
+ * 针对这些类型进行单独处理操作实现。所以需要区分开不同类型的元数据信息。
+ * */
+case class MetaData(in_type:String, driver:String, url:String,
+                     username:String, password:String, db_table:String,
+                     in_path:String, separator:String, in_fields:String,
+                     cond_fields:String, out_fields:String, out_path:String,
+                     zk_hosts:String, zk_port:String, hbase_table:String,
+                     family:String, select_field_names:String, where_field_names:String,
+                     where_field_values:String
+                   ){
+
+  /**
+   * 定义实现简单的义务逻辑实现
+   * */
+  def isHbase():Boolean={
+      in_type.toLowerCase()=="hbase"
+  }
+
+  /**
+   * 定义是否是mysql的数据类型
+   * */
+  def isRDBMS(): Boolean ={
+    in_type.toLowerCase=="mysql"||in_type.toLowerCase=="postgresql"||
+    in_type.toLowerCase=="oracle"||in_type.toLowerCase=="rdbms"
+  }
+
+  /***
+   *  定义是否是hdfs的数据类型信息
+   */
+  def isHdfs():Boolean={
+    in_type.toLowerCase()=="hdfs"
+  }
+
+  /**
+   * 将元数据转换为对应类型的元数据对象。转化成为hbase的metadata
+   * */
+  def  toHbaseMeta():HbaseMeta={
+    if(!isHbase()){
+       //  抛异常对应的是最为严谨的操作实现
+      null
+    }
+    if(StringUtils.isEmpty(in_fields)||StringUtils.isEmpty(out_fields)){
+      // 抛异常对应的是最为严谨的操作实现
+      null
+    }
+    val inFields: Array[String] = in_fields.split(",")
+    val outFields: Array[String] = out_fields.split(",")
+    HbaseMeta(CommonMeta(in_type,inFields,outFields),hbase_table,family)
+  }
+
+  /**
+   * 将数据转化为mysql的MySqlMeta的原始数据执行操作
+   * */
+  def toMySqlMeta():MySqlMeta={
+      if(!isRDBMS()){
+        null
+      }
+    if(StringUtils.isEmpty(in_fields)||StringUtils.isEmpty(out_fields)){
+      // 抛异常对应的是最为严谨的操作实现
+      null
+    }
+     //  判断转换操作
+    val inFields: Array[String] = in_fields.split(",")
+    val outFields: Array[String] = out_fields.split(",")
+    MySqlMeta(CommonMeta(in_type,inFields,outFields),driver,url,username,password,db_table)
+  }
+
+  /**
+   * 将原始数据转化成为hdfs的原始数据执行操作实现.HdfsMeta
+   * */
+  def toHdfsMeta():HdfsMeta={
+    if(!isHdfs()){
+      null
+    }
+    if(StringUtils.isEmpty(in_fields)||StringUtils.isEmpty(out_fields)){
+      // 抛异常对应的是最为严谨的操作实现
+      null
+    }
+    //  判断转换操作
+    val inFields: Array[String] = in_fields.split(",")
+    val outFields: Array[String] = out_fields.split(",")
+    HdfsMeta(CommonMeta(in_type,inFields,outFields),in_path,separator)
+  }
+}
+
+/**
+ * 下面是通用的数据类型
+ * */
+case class CommonMeta(inType:String,inFields:Array[String],outFields:Array[String])
+
+/**
+ * mysql的元数据信息
+ * */
+case class MySqlMeta(commonMeta: CommonMeta,driver:String,url:String,userName:String,password:String,tableName:String)
+
+/**
+ * hbase的元数据信息
+ * */
+case class HbaseMeta(commonMeta: CommonMeta,tableName:String,columnFamily:String)
+
+/**
+ * hdfs的元数据信息
+ * */
+case class HdfsMeta(commonMeta: CommonMeta,inPath:String,separator:String)
---
+++ tag_model_new/src/main/scala/cn/itcast/model/mtag/GenderModel.scala
@@ -0,0 +1,67 @@
+package cn.itcast.model.mtag
+
+import java.util.Properties
+
+import cn.itcast.model.{MetaData, Tag}
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+
+object GenderModel {
+
+  val  config=ConfigFactory.load()
+  val spark=SparkSession.builder()
+    .appName("gender mode")
+    .master("local[6]")
+    .getOrCreate()
+  val TAG_NAME="性别"
+
+  def main(args: Array[String]): Unit = {
+    //  读取mysql中的4级和5级标签数据
+    //  通过4级标签处理元数据
+    //  处理元数据，处理成为结构化的方式
+    //  使用元数据，连接原表的数据，匹配计算标签的数据。
+    // 将标签汇总，放入用户画像表数据？
+    //  结构赋值操作实现？
+    val (fourtag,fivetags)=readBasicTag()
+    // 处理元数据数据，获取的是4级标签的数据执行操作实现。
+    readMetaData(fourtag.id)
+  }
+
+  /**
+   * 读取源数据信息
+   * */
+  def  readMetaData(fourtagId:String):Unit={
+      import  spark.implicits._
+      import org.apache.spark.sql.functions._
+      //  读取元数据信息。元数据表的配置信息，解析元数据，以对象的方式返回数据信息？
+      val  url=config.getString("jdbc.meta_data.url")
+      val table: String = config.getString("jdbc.meta_data.table")
+      val matchColumn=config.getString("jdbc.meta_data.match_column")
+      // 读取元数据信息,将配置解析成为样例类对象的数据执行返回操作实现。
+      val head: MetaData = spark.read.jdbc(url, table, new Properties())
+        .where(col(matchColumn) === fourtagId).as[MetaData].collect().head
+    print(head)
+  }
+  /**
+   * 读取basicTag的基础数据。
+   * */
+  def readBasicTag():(Tag,Array[Tag])={
+      // 读取和读取配置文件：application.conf
+      // 创建sparkSession数据信息？读取四级标签
+      val url: String = config.getString("jdbc.basic_tag.url")
+      val table: String = config.getString("jdbc.basic_tag.table")
+      // 使用4级标签,对应的读取5级标签的配置操作。
+      // 将mysql的数据表读成了frame执行操作的。
+      val source: DataFrame = spark.read.jdbc(url, table, new Properties())
+      // 读取一个四级标签的数据信息的，只需要一个标签的数据的。通过name执行筛选操作的
+      //  使用column对象执行操作
+      import spark.implicits._
+      //  scala的相等操作.得到4级标签的数据信息
+      val fourTag: Tag = source.where('name === TAG_NAME).as[Tag].collect().head
+      //  得到多个五级标签的信息？
+      val fiveTags: Array[Tag] = source.where('pid === fourTag.id).as[Tag].collect()
+      //  得到五级标签的数据信息？
+      //  使用元祖模拟返回多个数值信息
+    (fourTag,fiveTags)
+  }
+}
