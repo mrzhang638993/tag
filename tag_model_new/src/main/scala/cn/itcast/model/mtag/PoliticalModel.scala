c74fc44898b17d4feb48a9692d0a16c582c0586b
mrzhang638993 <1446719386@qq.com>
2021-01-05 22:13:49 +0800

    抽取方法实践操作

---
+++ tag_model_new/src/main/scala/cn/itcast/model/mtag/PoliticalModel.scala
@@ -0,0 +1,36 @@
+package cn.itcast.model.mtag
+
+import cn.itcast.model.Tag
+import cn.itcast.model.utils.BasicModel
+import org.apache.spark.sql.{Column, DataFrame}
+
+object PoliticalModel extends BasicModel{
+  /**
+   * 获取对应的标签名称信息
+   **/
+  override def tagName(): String = {
+    ""
+  }
+
+  /**
+   * 处理方法和操作逻辑
+   **/
+  override def process(df: DataFrame, fiveTags: Array[Tag], outFields: Array[String]): DataFrame = {
+    //  导入spark对应的隐式转换
+    import spark.implicits._
+    import org.apache.spark.sql.functions._
+    //  执行匹配计算，生成条件列
+    var conditions:Column=null
+    for(tag<-fiveTags){
+      conditions=if(conditions==null){
+        when('politicalface===tag.rule,tag.id)
+      }else{
+        conditions.when('politicalface===tag.rule,tag.id)
+      }
+    }
+    conditions=conditions.as(outFields.head)
+    //  在source上面执行筛选，执行条件。
+    val frame: DataFrame = df.select('id, conditions)
+    frame
+  }
+}
