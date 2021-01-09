bf628f53c520b2fa06f603b421680a8b36ad6acc
mrzhang638993 <1446719386@qq.com>
2021-01-09 11:35:21 +0800

    获取最近一次支付的操作

---
+++ tag_model_new/src/main/scala/cn/itcast/model/stats/RecentPaymentCodeModel.scala
@@ -0,0 +1,50 @@
+package cn.itcast.model.stats
+
+import cn.itcast.model.Tag
+import cn.itcast.model.utils.BasicModel
+import org.apache.spark.sql.{Column, DataFrame, Dataset, Row}
+import org.apache.spark.sql.expressions.Window
+
+/**
+ * 统计最近一次的支付方式
+ * */
+object RecentPaymentCodeModel  extends  BasicModel{
+
+  def main(args: Array[String]): Unit = {
+       startFlow()
+  }
+  /**
+   * 获取对应的标签名称信息
+   **/
+  override def tagName(): String = "支付方式"
+
+  /**
+   * 处理方法和操作逻辑
+   **/
+  override def process(df: DataFrame, fiveTags: Array[Tag], outFields: Array[String]): DataFrame = {
+       //  导入spark的相关的依赖
+    import spark.implicits._
+    import org.apache.spark.sql.functions._
+      //  找到最近的一次的支付方式.获取时间信息
+    val dfClean: Dataset[Row] = df.filter($"paymentcode".isNotNull && length(trim($"paymentcode")) > 0)
+    dfClean.show()
+    val destValue: Dataset[Row] = dfClean.select('memberid as "id",
+        row_number() over Window.partitionBy('memberid).orderBy('paytime.desc) as "rn",
+        'paymentcode as "paymentcode"
+    ).where('rn === 1)
+    // 根据条件执行规则匹配操作
+    var conditions:Column=null;
+    for(tag<-fiveTags){
+      conditions=if(conditions==null){
+        when('paymentcode===tag.rule,tag.id)
+      }else{
+        conditions.when('paymentcode===tag.rule,tag.id)
+      }
+    }
+    conditions=conditions.as(outFields.head)
+    val frame: DataFrame = destValue.select('id, conditions)
+    //frame.show()
+    //frame
+    null
+  }
+}
