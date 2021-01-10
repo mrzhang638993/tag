c5fc2bdc355e3d3d83acbb13d1fce5a49a03e035
mrzhang638993 <1446719386@qq.com>
2021-01-10 16:58:54 +0800

    修改basicModel数据操作

--- tag_model_new/src/main/scala/cn/itcast/model/utils/BasicModel.scala
+++ tag_model_new/src/main/scala/cn/itcast/model/utils/BasicModel.scala
@@ -74,6 +74,7 @@ trait BasicModel {
     val table: String = config.getString("jdbc.meta_data.table")
     val df: DataFrame = spark.read.jdbc(metaUrl, table, new Properties())
     import spark.implicits._
+    df.sort('tag_id.desc).show()
     df.where('tag_id===fourTag.id).as[MetaData].collect().head
   }
   /**
