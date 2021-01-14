0e0a4ff538ed69a8a92665f55315eb64a0ebb52e
mrzhang638993 <33189223+mrzhang638993@users.noreply.github.com>
2021-01-14 09:54:40 +0800

    Update RMFPredictModel.scala

--- tag_model_new/src/main/scala/cn/itcast/model/ml/RMFPredictModel.scala
+++ tag_model_new/src/main/scala/cn/itcast/model/ml/RMFPredictModel.scala
@@ -39,10 +39,26 @@ object RMFPredictModel  extends  BasicModel{
     val sortedCenters: immutable.IndexedSeq[(Int, Double)] = model.clusterCenters.indices.map(i => (i, model.clusterCenters(i).toArray.sum)).sortBy(_._2).reverse
     //  得到序号的操作，对应的可以得到
     val centerIndex: DataFrame = sortedCenters.indices.map(i => (sortedCenters(i)._1, i + 1)).toDF("predict", "index")
+    // 下面预测的数据是错误的，需要重新编写代码进行预测操作实现？
+    // 步骤一：针对于fivetags的数据需要和centerIndex进行关联，得到关联关系管理
+    // val ruleTag: DataFrame = centerDf.join(five, "rule")
+    // val perdict: DataFrame = ruleTag.select(predictStr, "tagsId")  得到预测值和tagsId之间的关联关系？
+    // 下面得到预测值和映射关系之间的关联map数据信息？
+    //val perMap = perdict.map(t => {
+    // val pre = t.getAs(predictStr).toString
+    //   val tag = t.getAs("tagsId").toString
+    //   (pre, tag)
+   // }).collect().toMap
+    //  根据对应的预测数值获取到对应的tagId的数据信息？
+    //var predictUdf=udf((perdict:String)=>{
+    //  var tag=perMap(perdict)
+    // tag
+    //})
+    val new_Tag = prodicted.select('memberId as "userId", predictUdf('predict) as "tagsId")
     //  join的时候一个表的数据特别的小的话，会自动的进行join的map端的优化操作的。
-    val frame1: DataFrame = prodicted.join(centerIndex, prodicted.col("predict") === centerIndex.col("predict"))
-      .select(prodicted.col("id"), centerIndex.col("index") as outFields.head)
-    frame1.show()
-    frame1
+    //val frame1: DataFrame = prodicted.join(centerIndex, prodicted.col("predict") === centerIndex.col("predict"))
+     // .select(prodicted.col("id"), centerIndex.col("index") as outFields.head)
+    //frame1.show()
+    //frame1
   }
 }
